1629ee7f11a59488c71a4190c9f55094c955602a
lamjadndour@gmail.com <lamjadndour@gmail.com>
2020-12-01 16:02:29 +0100

    Atelier 2 is done

--- atelier1,2/Atelier2/Main.java
+++ atelier1,2/Atelier2/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class Main {
 	//ex 1
+	static Scanner input = new Scanner(System.in);
+
 	static void welcome() {
 		String text1 = "Quel est votre nom";
 		String text2 = "Quel est votre prénom";
@@ -67,7 +69,6 @@ public class Main {
 		System.out.println("Bonjour " + nom + " "+prenom +", tu as " + age + " ans. \ntu es "+text+" et vous êtes " + text2);
 	}
 	static void sayMyName() {
-		Scanner input = new Scanner(System.in);
 		String nom = "";
 		String prenom = "";
 		boolean genre = false;
@@ -88,46 +89,57 @@ public class Main {
 	
 	
 	//ex 5 
-	//ex 4
 		private static void sayMyGenderAndAge(String genre, int age) {
 			String text = "majeur";
-			String text2 = "tu peux ";
+			String text2 = " tu peux ";
 
 			if(age < 18) {
 				text = "mineur";
-				text2 = "tu ne peux pas ";
+				text2 = " tu ne peux pas ";
 			}
-			if(genre = "Homme") {
-				System.out.println("Bonjour Monsieur "+", vous etez " + text+text2+'passer');
+			if(genre == "homme") {
+				System.out.println("Bonjour Monsieur "+" vous etez " + text+text2+"passer");
 				  
-			}else if (genre = "Femme") 
-				System.out.println("Bonjour Madmosielle "+", vous etez " + text+text2+'passer');
+			}else if (genre == "femme") 
+				System.out.println("Bonjour Mll "+", vous etez " + text+text2+"passer");
+			else System.out.println("erreur in 2nd ft");
+				
 			
-			System.out.println("Bonjour " + nom + " "+prenom +", tu as " + age + " ans. \ntu es "+text+" et vous êtes " + text2);
 		}
-		static void sayMyName() {
-			Scanner input = new Scanner(System.in);
-			String nom = "";
-			String prenom = "";
-			boolean genre = false;
-			int age;
-			System.out.println("entre Votre nom");
-			nom = input.nextLine();
-			System.out.println("entre Votre prénom");
-			prenom = input.nextLine();
-			System.out.println("entre année de naissance");
-			age = input.nextInt();
-			age = 2020 - age;
-			System.out.println("Si homme entrè 1, sinon 2");
-			if(input.nextInt() == 1) 
-				genre = true;
+		static void sayMyGender() {
+			int genre = 0;
+			int age = 0;
+			String text = "homme";
+			boolean status = true;
+			while(status) {
+				System.out.println("veuillez entrer le genre: \nvous un homme :1 \nune femme :2");
+				genre = input.nextInt();
+				if(genre == 1 || genre == 2) {
+					status = false;
+					if(genre == 2)
+						text = "femme";
+				}else {
+					System.out.println("erreur!! : genre incorrect");
+				}
+
+			}
+			status = true;
+			while(status) {
+				System.out.println("entre année de naissance");
+				age = input.nextInt();
+				if(age < 2015 && age > 1890) {
+					age = 2020 - age;
+					status = false;
+				}else
+					System.out.println("erreur!! : année incorrect");
+			}
 			
-			sayMynameAndAge(nom, prenom, age, genre);
+			sayMyGenderAndAge(text,age);
 		}
 		
 	public static void main (String[] args) {
 		
-		sayMyName();
+		sayMyGender();
 	}
 	
 }
